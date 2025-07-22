52de5d4b66ab9c9beb5cae1e4aa0482fd339e72c
Rebul Bilgi Sistemleri <99711479+rebulinformation@users.noreply.github.com>
2025-07-22 10:47:38 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/manifest.xml", // veya manifest.txt
+        source: "/manifest.txt"
         headers: [
           {
             key: "Content-Type",
